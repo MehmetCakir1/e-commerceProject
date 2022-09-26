f013a3521d6efe82489a2d7d0b3f4af089f9b6b4
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-09-26 18:17:18 +0300

    react-slick added

--- src/components/FeaturedProduct.jsx
+++ src/components/FeaturedProduct.jsx
@@ -1,34 +1,31 @@
 import { useContext, useState } from "react";
 import { TiZoomOutline } from "react-icons/ti";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'
 import { ProductContext } from "../context/ProductContext";
-
-
 const FeaturedProduct = ({item}) => {
-  const {costing}=useContext(ProductContext)
     const [zoom,setZoom]=useState(false)
     const navigate=useNavigate()
     const { id, name, price, image } = item;
+    const {costing}=useContext(ProductContext)
   return (
-    <div key={id} className="featured-card glide__slide slider">
+    <div key={id} className="featured-card">
     <div>
       <div className="featured-imgDiv" onMouseOver={()=>setZoom(true)} onMouseOut={()=>setZoom(false)}>
         <img src={image} alt={name} />
         {
           zoom && <div className="featured-zoom d-flex align-items-center justify-content-center">
-          <button className="border-0 rounded-circle p-2" onClick={()=>navigate(`/products/${id}`)}><TiZoomOutline className="text-light fs-3"/></button> 
+          <button className="border-0 rounded-circle p-2" onClick={()=>navigate("/products/"+id )}><TiZoomOutline className="text-light fs-3"/></button>
         </div>
         }
       </div>
       <div className="d-flex align-items-center justify-content-between mt-3">
         <span className="text-capitalize">{name}</span>
         <span>
-          {costing(price)}
+        ${costing(price)}
         </span>
       </div>
     </div>
   </div>
   )
 }
-
 export default FeaturedProduct
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,56 +1,72 @@
 import React from "react";
 import { useContext } from "react";
 import { ProductContext } from "../context/ProductContext";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import FeaturedProduct from "./FeaturedProduct";
-// import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y, } from 'swiper';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import Glider from "react-glider";
+// import "glider-js/glider.min.css";
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import Slider from "react-slick";
+
 
 const Main = () => {
-  const { featured} = useContext(ProductContext);
-  const navigate=useNavigate()
-  // console.log(featured.slice(0,3));
-  // console.log(zoom)
+
+  const { featured } = useContext(ProductContext);
+  const navigate = useNavigate();
+  
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 578,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+    ]
+  };
   return (
     <main className="py-5 featured mt-3 mt-md-5">
-    <h1 className="text-center fw-bold mt-2 main-title">Featured Products</h1>
-    <div className="underline m-auto mb-3"></div>
-    {/* <div className="container row m-auto p-0"> */}
-  <Swiper
-    // install Swiper modules
-    modules={[Navigation, Pagination, Scrollbar, A11y]}
-    spaceBetween={90}
-    slidesPerView={3}
-    slidesPerGroup={3}
-    // loop={true}
-    // loopFillGroupWithBlank={true}
-    // navigation
-    pagination={{ 
-      clickable: true,
-    }}
-    className="row container m-auto px-4 d-flex justify-content-center align-items-center"
-  >
-           {featured.map((item) => {
-        return (
-          <SwiperSlide className="col-4"><FeaturedProduct key={item.id} item={item}/>
-          </SwiperSlide>
-        );
-      })}
-  </Swiper>
-    {/* </div> */}
-    <div className="text-center">  <button className="text-uppercase py-1 px-3 border-0 rounded-2 mt-4" onClick={()=>navigate("/products")} >All Products</button></div>
-  </main>
+      <h1 className="text-center fw-bold mt-2 main-title">Featured Products</h1>
+      <div className="underline m-auto mb-3"></div>
+      
+        {/* //! slider */}
+        <Slider {...settings} className="container w-75 w-md-100 m-auto p-0 ">
+                {featured.map((item) => {
+              return (
+                <ul className="m-0 p-0 featured-ul m-auto">
+                <li className="list-unstyled mx-1 "><FeaturedProduct key={item.id} item={item} /></li>
+                </ul>
+                );
+              })}
+      </Slider>   
+      <div className="text-center">
+        {" "}
+        <button
+          className="text-uppercase text-white py-1 px-3 border-0 rounded-2 mt-4"
+          onClick={() => navigate("/products")}
+        >
+          All Products
+        </button>
+      </div>
+    </main>
   );
 };
-
 export default Main;
-
-
